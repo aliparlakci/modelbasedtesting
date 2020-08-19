c16094015e2db59932c29ceb1a7de7926885d5ab
Ali Parlakci <ali.parlakci@piworks.net>
2020-08-19 14:24:44 +0300

    Initial commit

---
+++ ModelBasedTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModelBasedTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
