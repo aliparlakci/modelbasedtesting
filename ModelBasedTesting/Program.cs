36acbc66f36cc407d90e09f033e39753f04f6bb8
Ali Parlakci <ali.parlakci@piworks.net>
2020-09-04 11:22:32 +0300

    makes it simple

--- ModelBasedTesting/Program.cs
+++ ModelBasedTesting/Program.cs
@@ -2,43 +2,22 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Linq;
 using ModelBasedTesting.SharedStates;
 using ModelBasedTesting.Helpers;
-using Microsoft.Extensions.Logging;
-
 namespace ModelBasedTesting
 {
     class Program
     {
-        static int Main(string[] args)
+        static void Main(string[] args)
         {
-            ServiceCollection serviceCollection = new ServiceCollection();
-            ConfigureServices(serviceCollection);
-
-            ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
-
             if (args.Length == 0)
             {
                 System.Console.WriteLine("Please enter the path and file name for the model");
-                return 1;
+                return;
             }
 
-            return run(args[0]);
-
-        }
-
-        private static void ConfigureServices(IServiceCollection services)
-        {
-            services.AddLogging(configure => configure.AddConsole())
-                .Configure<LoggerFilterOptions>(
-                    options => options.MinLevel = LogLevel.Information
-                );
-        }
-
-        static int run(string modelLocation)
-        {
+            string modelLocation = args[0];
 
             Browser browser = new Browser();
 
@@ -53,7 +32,7 @@ namespace ModelBasedTesting
             {
                 JObject nextStep = GraphWalkerClient.getNext();
 
-                string className = $"{nextStep.GetValue("modelName").ToString()}";
+                string className = nextStep.GetValue("modelName").ToString();
                 Type modelClass = sharedStates[className];
                 ConstructorInfo constructor = modelClass.GetConstructor(new Type[] { typeof(Browser) });
 
@@ -62,26 +41,10 @@ namespace ModelBasedTesting
                     Console.WriteLine($"Model and element to be called: {nextStep.GetValue("modelName").ToString()}.{nextStep.GetValue("currentElementName").ToString()}");
 
                     object instance = constructor.Invoke(new object[] { browser });
-
                     MethodInfo methodInfo = modelClass.GetMethod(nextStep.GetValue("currentElementName").ToString());
-                    try
-                    {
-                        methodInfo.Invoke(instance, new object[] { });
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"    ERROR: {e.InnerException.Message}");
-                        throw e.InnerException;
-                        return 1;
-                    }
-                    finally
-                    {
-                    }
-
+                    methodInfo.Invoke(instance, new object[] { });
                 }
             }
-
-            return 0;
         }
     }
 }
